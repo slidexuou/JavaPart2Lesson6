0c123f03a918884571fe40bdd42e7d342286f59d
slidexuou <slidexuou7@gmail.com>
2021-04-06 16:39:13 +0700

    first commit

---
+++ charToString.java
@@ -0,0 +1,9 @@
+package com.xuou.charToStr;
+
+public class charToString {
+public static void main(String[]args) {
+	char[] myChar = {'S','L','I','D','E'};
+	String myText = new String(myChar);
+	System.out.println(myText);
+}
+}
